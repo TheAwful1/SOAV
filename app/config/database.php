eba33049d8b3e01d7576f0e883dfb158cb050b01
Edwin Felipe <105158452+TheAwful1@users.noreply.github.com>
2025-11-03 12:23:01 -0400

    Segundo commit

--- app/config/database.php
+++ app/config/database.php
@@ -1,16 +1,26 @@
 <!--Aqui se hace la conexion a la base de datos -->
 
 <?php
-$host = 'localhost';
-$dbname = 'soav';
-$username = 'root';
-$password = '';
+class Database{
+private $host = 'localhost';
+private $dbname = 'soav';
+private $username = 'root';
+private $password = '';
+
+private $pdo;
+
+public function connect(){
 
 try {
-  $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $this->pdo = new PDO("mysql:host={$this->host};dbname={$this->dbname};charset=utf8",
+   $this->username,
+   $this->password
+  );
+  $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  return $this->pdo;
 } catch (PDOException $e) {
   die("Error en la conexión: " . $e->getMessage());
 }
-
+}
+}
 ?>
--- app/models/User.php
+++ app/models/User.php
@@ -10,15 +10,17 @@ class Usuario {
 
 
   public function registrar($nombre,$email,$password,$rol = 'cliente'){
-    $sql = "INSERT INTO usuarios (nombre, email, password, rol, state) 
-        VALUES (?, ?, ?, ?, 'activo')";
-    $stmt = $this->pdo->prepare($sql);
+    $hash = password_hash($password, PASSWORD_DEFAULT); //Esto de aqui lo que hace es usar un algoritmo para fortalecer las contraseñas.
+
+    $stmt = $this->pdo->prepare("INSERT INTO usuarios (nombre, email, password, rol, state) 
+        VALUES (?, ?, ?, ?, 'activo')");
     return $stmt->execute([$nombre, $email, password_hash($password, PASSWORD_BCRYPT), $rol]);
   }
   
 
   public function login($email, $password)
   {
+    
     $stmt = $this->pdo->prepare("");
     $stmt->execute([$email]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -26,9 +28,25 @@ class Usuario {
         return $user;
     }
     return false;
-  }
+  }//Aqui faltan las demas operaciones CRUD
+//Podria añadir una forma de hacer que solo se desactivaran usuarios o vehiculos sin eliminarlos de la base de datos
+public function verTodos(){
+  $sql = "SELECT * FROM usuarios";
 }
-//Aqui faltan las demas operaciones CRUD
+public function buscar($id){
+  $stmt = $this->pdo->prepare( "SELECT * FROM usuarios WHERE id = ?");
+  $stmt->execute([$id]);
+
+}
+public function editar($nombre, $email, $password, $id){
+  $sql = "UPDATE TABLE usuarios (nombre, email, password)
+  VALUES(?,?,?)";
+}//Aaui se pueden editar los datos de los usuarios que se presenten en la busqueda, y editando los datos en el front se puede enviar un formulario que tenga los datos que se requieran 
+public function eliminar($id){
+  
+}
+}
+
 
 
 ?>
